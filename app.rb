dc25ddc60d64a3583d19868debe7612a6d972c1a
Kelsey Langlois and LDMacKrell <ldmackrell@>
2017-09-26 09:38:11 -0700

    Add UI for manager to add products.

--- app.rb
+++ app.rb
@@ -10,3 +10,23 @@ require "pry"
 get('/') do
   erb(:index)
 end
+
+get('/manager') do
+  @products = Product.all
+  erb(:manager)
+end
+
+post('/manager') do
+  # Product.create({:description => params.fetch('description'), :price => params.fetch('price').to_f})
+  Product.create({description: params['description'], price: params['price'].to_f})
+  redirect '/manager'
+end
+
+get('/cashier') do
+  @products = Product.all
+  erb(:cashier)
+end
+
+post('/cashier') do
+  erb(:cashier)
+end
